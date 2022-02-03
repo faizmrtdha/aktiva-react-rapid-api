4b6c0c28211ae40fadf2f842c8fe177b58c52b87
Faiz Murtadha Nur Wahhab <faizmrtdha99@gmail.com>
2022-02-03 12:38:32 +0700

    Add icon coin and fix coin price decimal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import { coinsAPI } from "./hooks/API";
-import { Card, Container } from "react-bootstrap";
+import { Card, Col, Container, Image, Row } from "react-bootstrap";
 
 function App() {
   const [coins, setCoins] = useState([]);
   const showCoins = async () => {
     try {
       const response = await coinsAPI;
-      setCoins(response.data.data.coins);
+      const data = await response.data.data.coins;
+      data.forEach((element) => {
+        element.price = parseFloat(element.price).toFixed(2);
+      });
+      setCoins(data);
     } catch (e) {
       console.log(e);
     }
@@ -19,14 +23,24 @@ function App() {
   return (
     <div className="app">
       <Container>
+        <h1>Global Crypto Stats</h1>
+        <Row>
+          <Col>
+            <span>Total Cryptocurrencies</span>
+          </Col>
+          <Col>Total Exchange</Col>
+        </Row>
         <div className="rank">
           {coins.map((coin, index) => (
             <Card className="card-rank">
               <Card.Body>
-                <Card.Title key={coin.rank}>{coin.name}</Card.Title>
-                <Card.Text>Price : {coin.price}</Card.Text>
+                <Card.Title key={coin.rank} className="title-card">
+                  <span>{coin.name}</span>
+                  <Image src={coin.iconUrl} fluid width={24} height={24} />
+                </Card.Title>
+                <Card.Text>Price : ${coin.price}</Card.Text>
                 <Card.Text>Market Cap : {coin.marketCap}</Card.Text>
-                <Card.Text>Daily Change : {coin.change}</Card.Text>
+                <Card.Text>Daily Change : {coin.change}%</Card.Text>
               </Card.Body>
             </Card>
           ))}
