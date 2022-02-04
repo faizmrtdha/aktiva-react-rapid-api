b2db9f5dee5b5c2f3c4ff4154e41f98c4db89505
Faiz Murtadha Nur Wahhab <faizmrtdha99@gmail.com>
2022-02-04 13:03:42 +0700

    Add stats global

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import { coinsAPI } from "./hooks/API";
+import { coinsAPI, cryptosAPI } from "./hooks/API";
 import { Card, Col, Container, Image, Row } from "react-bootstrap";
 
 function App() {
   const [coins, setCoins] = useState([]);
+  const [globals, setGlobals] = useState([]);
+
   const showCoins = async () => {
     try {
       const response = await coinsAPI;
@@ -17,24 +19,47 @@ function App() {
     }
   };
 
+  const cryptos = async () => {
+    try {
+      const response = await cryptosAPI;
+      setGlobals(response.data.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   useEffect(() => {
     showCoins();
+    cryptos();
   });
   return (
     <div className="app">
       <Container>
-        <h1>Global Crypto Stats</h1>
-        <Row>
+        <h1 className="title-global">Global Crypto Stats</h1>
+        <Row className="row-stats">
           <Col>
-            <span>Total Cryptocurrencies</span>
+            <h6 className="title-stats">Total Cryptocurrencies</h6>
+            <p className="p-stats">{globals.totalCoins}</p>
+            <h6 className="title-stats">Total Market Cap</h6>
+            <p className="p-stats">{globals.totalMarketCap}</p>
+            <h6 className="title-stats">Total Cryptocurrencies</h6>
+            <p className="p-stats">{globals.totalCoins}</p>
           </Col>
-          <Col>Total Exchange</Col>
+          <Col>
+            <h6 className="title-stats">Total Exchange</h6>
+            <p className="p-stats">{globals.totalExchanges}</p>
+            <h6 className="title-stats">Total 24h Volume</h6>
+            <p className="p-stats">{globals.total24hVolume}</p>
+            <h6 className="title-stats">Total Markets</h6>
+            <p className="p-stats">{globals.totalMarkets}</p>
+          </Col>
         </Row>
+        <h2>Top 10 Cryptos In The World</h2>
         <div className="rank">
           {coins.map((coin, index) => (
-            <Card className="card-rank">
+            <Card className="card-rank" key={coin.rank}>
               <Card.Body>
-                <Card.Title key={coin.rank} className="title-card">
+                <Card.Title className="title-card">
                   <span>{coin.name}</span>
                   <Image src={coin.iconUrl} fluid width={24} height={24} />
                 </Card.Title>
